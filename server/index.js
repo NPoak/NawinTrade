380b5638c4ead2caaeeeb83081fe22be6d590dd8
NopeNH <nopz3456z@gmail.com>
2024-05-02 00:36:03 +0700

    TryInsertToStocksTable
    Tam mai mai dai waa

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import cors from "cors";
 import financialModelingPrep from 'financialmodelingprep';
 import mysql from "mysql";
+import dbRoutes from './routes/createDB.route.js'; 
 
 
 const app = express();
@@ -11,56 +12,67 @@ const port = process.env.PORT || 5000;
 const apiKey = "gQERlMvVTI5GZJtzaVkQgSLTBpXiuxW7";
 
 const fmp = financialModelingPrep(apiKey);
-
+app.use(express.json());
 
 
 
 app.use(cors());
 
 
-app.get('/', (req, res) => {
-    console.log(req);
-    return response.status(234).send("Hello Nawin");
+// app.get('/', (req, res) => {
+//     console.log(req);
+//     return res.status(234).send("Hello Nawin");
+// });
+
+app.use('/api/eDB', dbRoutes);
+
+const pool  = mysql.createPool({
+  host: 'nawintrade-db.cta0k8w6kiam.ap-southeast-2.rds.amazonaws.com',
+  port: '3306',
+  user: 'admin',
+  password: 'Bhun123456',
+  database: 'nawintradeDB'
 });
+
+
+
+
 
 app.get('/getstockdata', async (req, res) => {
   try {
-  const response = await fmp.stock('aapl').history();
-  res.json(response); 
+  const response = await fmp.stock('aapl').profile();
+  res.json(response);
+   
   } catch(error) {
     console.error('Error fetching stock quote:', error);
   }
 } );
 
 
-const pool  = mysql.createPool({
-  connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password: null,
-  database: 'dreamhome'
-});
 
 
 
-app.get('/dreamhomestaff',  (req, res) => {
-  pool.getConnection((err, connection) => {
-    if(err) throw err
-    console.log('connected as id ' + connection.threadId)
-    connection.query('SELECT * from staff', (err, rows) => {
-        connection.release() // return the connection to pool
 
-        if (!err) {
-            res.send(rows)
-        } else {
-            console.log(err)
-        }
 
-        // if(err) throw err
-        console.log('The data from staff table are: \n', rows)
-    })
-})
-})
+// app.get('/createBrokerTable',  (req, res) => {
+//   pool.getConnection((err, connection) => {
+//     if(err) throw err
+//     console.log('connected as id ' + connection.threadId)
+//     connection.query('SHOW TABLES', (err, rows) => {
+//         connection.release() // return the connection to pool
+
+//         if (!err) {
+//           //console.log("table created")
+//             //res.send(rows)
+//         } else {
+//             console.log(err)
+//         }
+//         //console.log("Table created!")
+//         // if(err) throw err
+//         console.log('The data from staff table are: \n', rows)
+//     })
+// })
+// })
 
 
 
