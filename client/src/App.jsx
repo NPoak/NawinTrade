17a97da32a87d7568aab378762b01f2c880645c1
KaEnds <kenza.ke15@gmail.com>
2024-05-02 16:57:47 +0700

    login-page no navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-
+import Loginpage from "./components/loginpage/Loginpage"
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl font-bold underline">
-      nawin trade!
-    </h1>
-    <p className="btn btn-primary">Button</p>
-    <button className="btn w-64 rounded-full">Button</button>
+      <Loginpage />
     </>
   )
 }
