732447ecf6c4213825fe7cb74f57f0c246bc37ae
xXKAYFISTXx <oak0809@gmail.com>
2024-05-04 23:41:29 +0700

    create makeOrder function

--- server/controllers/customer.controller.js
+++ server/controllers/customer.controller.js
@@ -1,5 +1,8 @@
+import financialModelingPrep from 'financialmodelingprep';
 import dbpool from '../db/connectAWSdb.js';
 import jwt from "jsonwebtoken";
+import MOMENT from 'moment'
+
 // stockView controller
 export const stockView = (req, res) => {
     const { StockSymbol, cookies } = req.body;
@@ -93,3 +96,69 @@ export const stockView = (req, res) => {
     });
 };
 
+//makeOrder controller
+export const makeOrder = async (req, res) =>{
+    const currentdate = MOMENT().format('YYYY-MM-DD HH:mm:ss');
+    console.log(currentdate)
+    const { StockSymbol,BuyHowMuch,AccountBalance,OrderType,cookies } = req.body;
+    const apiKey = "gQERlMvVTI5GZJtzaVkQgSLTBpXiuxW7";
+    const fmp = financialModelingPrep(apiKey);
+    const stockjson = await fmp.stock('AAPL').current_price();
+    const payload = jwt.verify(cookies, 'Bhun-er')
+    const userID = payload['userID']
+    
+    // console.log(cookies)
+    // console.log(payload['userID'])
+    
+    // console.log(stockjson['companiesPriceList'][0]['price'])
+    // res.send("stockjson")
+
+    dbpool.getConnection((err, connection) => {
+        if (err) throw err;
+        let com
+        const query_com =  `SELECT TradingComFee From Brokers WHERE BrokerID = (SELECT BrokerID FROM Users WHERE UserID = ? )`
+        connection.query(query_com,[userID],async(err,rows)=>{
+            if (err) throw err;
+            com = rows[0];
+            console.log(com['TradingComFee'])
+            if (!com) {
+                connection.release();
+                return res.status(400).json({ error: "Cannot get data" });
+            }
+            const RealNoCom = BuyHowMuch*(1-(com['TradingComFee']/100));
+            console.log(RealNoCom)
+            const Volume = RealNoCom/stockjson['companiesPriceList'][0]['price']  
+            if (err) throw err;
+            let stock;
+            const query_StockID = `SELECT * FROM Stocks WHERE StockSymbol = ?`;
+            connection.query(query_StockID, [StockSymbol], async(err, rows) => {
+                if (err) throw err;
+                stock = rows[0];
+                // console.log(stock['StockID'],OrderType,Volume);
+                
+                if (!stock) {
+                    connection.release();
+                    return res.status(400).json({ error: "Cannot get data" });
+                }
+                const query_Order = `INSERT INTO Orders (UserID, StockID, OrderType, Volume, Price, OrderStatus, OrderDateTime) VALUES (?,?,?,?,?,"Pending",?)`
+                connection.query(query_Order,[userID,stock['StockID'],OrderType,Volume,stockjson['companiesPriceList'][0]['price'],currentdate], (err,result)=>{
+                    if (err) throw err
+                    console.log("Insert Complete")
+                    // res.status(200).send("Insert Complete")
+                })
+            })
+        }) 
+        
+        const query_balance =  `UPDATE Users SET AccountBalance = ?  WHERE UserID = ?`
+        const money = AccountBalance-BuyHowMuch
+        connection.query(query_balance,[money,userID],async(err,rows)=>{
+            if (err) throw err;
+            console.log(money)
+            connection.release()   
+            res.status(200).send("Balance Updated")
+                 
+        }) 
+
+    })
+}
+    
--- server/routes/customer.route.js
+++ server/routes/customer.route.js
@@ -1,10 +1,10 @@
 import express, { Router } from "express";
-import { stockView } from "../controllers/customer.controller.js";
+import { stockView, makeOrder } from "../controllers/customer.controller.js";
 
 const router = express.Router();
 
 router.post('/stockView', stockView);
-
+router.post('/makeOrder',makeOrder);
 
 
 export default router
