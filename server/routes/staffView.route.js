0235f61c9f50e8e36b41e43bc07a6c97d4641fba
NopeNH <nopz3456z@gmail.com>
2024-05-17 02:12:22 +0700

    add staff orderApprove api (make/view) a

--- server/routes/staffView.route.js
+++ server/routes/staffView.route.js
@@ -1,9 +1,10 @@
 import express  from "express";
-import { staffProfile } from "../controllers/staffView.controller.js";
+import { staffOrderView, staffProfile } from "../controllers/staffView.controller.js";
 
 
 const router = express.Router();
 
 router.post('/staffProfile', staffProfile);
+router.post('/staffOrderView', staffOrderView)
 
 export default router
