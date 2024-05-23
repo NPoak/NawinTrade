4714320f982554180ab03b36a8230a249bb4f683
bloodMoon <frost4459@gmail.com>
2024-05-23 21:17:28 +0700

    trading23/5/67

--- client/src/components/Navbar/landing.jsx
+++ client/src/components/Navbar/landing.jsx
@@ -4,7 +4,7 @@ function Navbar(){
   <div className='navbar border-b-2 border-black'>
         <div className='flex justify-between w-full px-10'>
             <div>
-                <button><b><a className='nt_logo'>Nawin Trade</a></b></button>
+                <button><b><a className='nt_logo font-bold'>Nawin Trade</a></b></button>
                 <button><a href='/stockview/AAPL' className='menu'>ซื้อขายหุ้น</a></button>
                 <button><a className='menu'>เปิดพอร์ต</a></button>
                 <button><a className='menu'>ข่าวสาร</a></button>
---
+++ client/src/pages/trading_history/Trading_history.jsx
@@ -0,0 +1,9 @@
+import './Trading_history.css'
+import Navbar_Login from '../../components/Navbar/login'
+
+function Tradhist (){
+    
+    return(
+        <Navbar_Login/>
+    )
+}
